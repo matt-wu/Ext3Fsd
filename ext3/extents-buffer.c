e41d1a50d7e2d2adf99ca1c223c357b2b55b681f
ngkaho1234 <ngkaho1234@gmail.com>
2015-04-25 16:49:54 +0800

    Extents-buffer.c get reference count added.

--- ext3/extents-buffer.c
+++ ext3/extents-buffer.c
@@ -1,50 +1,78 @@
 #include <ext2fs.h>
 #include <linux/errno.h>
 
-static kmem_cache_t *extents_cache;
+#define BUFFER_SIZE sizeof(struct buffer_head)
 
+static NPAGED_LOOKASIDE_LIST extents_cache;
+static atomic_t              extents_cache_alloc_count;
+
+/*
+ * Initialize extents_cache so that we can allocate buffer
+ * header quickly.
+ */
 int ext4_init_extents_bh()
 {
-    extents_cache = kmem_cache_create(
-                         "extents_cache",   /* bh */
-                         sizeof(struct buffer_head),
-                         0,		        /* offset */
-                         SLAB_TEMPORARY,	/* flags */
-                         NULL);		    /* ctor */
-    if (extents_cache == NULL) {
-        printk(KERN_EMERG "JBD: failed to create extents handle cache\n");
-        return -ENOMEM;
-    }
+    ExInitializeNPagedLookasideList(
+        &extents_cache,
+        NULL,
+        NULL,
+        0,
+        BUFFER_SIZE,
+        'BTXE',
+        0);
     return 0;
 }
 
+/*
+ * Destroy extents_cache before the driver goes down.
+ */
 void ext4_destroy_extents_bh()
 {
-    if (extents_cache) {
-        kmem_cache_destroy(extents_cache);
-        extents_cache = NULL;
+    int buffer_alloc_count = 0;
+    ExDeleteNPagedLookasideList(&extents_cache);
+    if ((buffer_alloc_count = atomic_read(&extents_cache_alloc_count)) != 0) {
+        DbgPrint("EXTENTS_CACHE WARNING: There is buffer yet to be freed !!!\
+                  Count: %d\n", buffer_alloc_count);
+        DbgBreak();
     }
 }
 
+/*
+ * Allocate a new buffer header and zero it.
+ */
 static struct buffer_head *extents_new_buffer_head()
 {
-    struct buffer_head * bh = NULL;
-    bh = kmem_cache_alloc(extents_cache, GFP_NOFS);
+    PVOID  bh = NULL;
+    bh = ExAllocateFromNPagedLookasideList(&extents_cache);
     if (bh) {
-        memset(bh, 0, sizeof(struct buffer_head));
+        memset(bh, 0, BUFFER_SIZE);
         DEBUG(DL_BH, ("bh=%p allocated.\n", bh));
+        atomic_inc(&extents_cache_alloc_count);
     }
     return bh;
 }
 
+/*
+ * Free the buffer header.
+ */
 static void extents_free_buffer_head(struct buffer_head * bh)
 {
     if (bh) {
         DEBUG(DL_BH, ("bh=%p freed.\n", bh));
-        kmem_cache_free(extents_cache, bh);
+        ExFreeToNPagedLookasideList(&extents_cache, bh);
+        atomic_dec(&extents_cache_alloc_count);
     }
 }
 
+/*
+ * extents_bread: This function is a wrapper of CcPinRead routine.
+ * 
+ * @sb:    the device we need to undergo buffered IO on.
+ * @block: the block we want to read from.
+ *
+ * If the call to this routine succeeds, the pages underlying the buffer header
+ * will be locked into memory, so that the buffer header returned for use is safe.
+ */
 struct buffer_head *
 extents_bread(struct super_block *sb, sector_t block)
 {
@@ -98,6 +126,12 @@ errorout:
     return bh;
 }
 
+/*
+ * extents_bwrite: This function is a wrapper of CcPreparePinWrite routine.
+ * 
+ * @sb:    the device we need to undergo buffered IO on.
+ * @block: the block we want to write to.
+ */
 struct buffer_head *
 extents_bwrite(struct super_block *sb, sector_t block)
 {
@@ -152,11 +186,24 @@ errorout:
     return bh;
 }
 
+/*
+ * extents_mark_buffer_dirty: Mark the buffer dirtied and so
+ *                            that changes will be written back.
+ * 
+ * @bh: The corresponding buffer header that is modified.
+ */
 void extents_mark_buffer_dirty(struct buffer_head *bh)
 {
     set_buffer_dirty(bh);
 }
 
+/*
+ * extents_brelse: Release the corresponding buffer header.
+ *
+ * @bh: The corresponding buffer header that is going to be freed.
+ *
+ * The pages underlying the buffer header will be unlocked.
+ */
 void extents_brelse(struct buffer_head *bh)
 {
     struct block_device *bdev;
