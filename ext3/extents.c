9b982bef76e990d9d25888f55b00a87809080ffd
ngkaho1234 <ngkaho1234@gmail.com>
2015-04-27 14:04:37 +0000

    Extents.c function prototype got moved.

--- ext3/extents.c
+++ ext3/extents.c
@@ -157,28 +157,6 @@ static inline int ext4_ext_space_root_idx(struct inode *inode, int check)
 	return size;
 }
 
-static int ext4_split_extent_at(void *icb,
-			     handle_t *handle,
-			     struct inode *inode,
-			     struct ext4_ext_path **ppath,
-			     ext4_lblk_t split,
-			     int split_flag,
-			     int flags);
-
-static inline int
-ext4_force_split_extent_at(void *icb, handle_t *handle, struct inode *inode,
-		struct ext4_ext_path **ppath, ext4_lblk_t lblk,
-		int nofail)
-{
-	struct ext4_ext_path *path = *ppath;
-	int unwritten = ext4_ext_is_unwritten(path[path->p_depth].p_ext);
-
-	return ext4_split_extent_at(icb, handle, inode, ppath, lblk, unwritten ?
-			EXT4_EXT_MARK_UNWRIT1|EXT4_EXT_MARK_UNWRIT2 : 0,
-			EXT4_EX_NOCACHE | EXT4_GET_BLOCKS_PRE_IO |
-			(nofail ? EXT4_GET_BLOCKS_METADATA_NOFAIL:0));
-}
-
 static int
 ext4_ext_max_entries(struct inode *inode, int depth)
 {
@@ -266,6 +244,28 @@ static void ext4_extent_block_csum_set(struct inode *inode,
 	tail = find_ext4_extent_tail(eh);
 	tail->et_checksum = ext4_ext_block_csum(
 			inode, eh);
+}
+
+static int ext4_split_extent_at(void *icb,
+			     handle_t *handle,
+			     struct inode *inode,
+			     struct ext4_ext_path **ppath,
+			     ext4_lblk_t split,
+			     int split_flag,
+			     int flags);
+
+static inline int
+ext4_force_split_extent_at(void *icb, handle_t *handle, struct inode *inode,
+		struct ext4_ext_path **ppath, ext4_lblk_t lblk,
+		int nofail)
+{
+	struct ext4_ext_path *path = *ppath;
+	int unwritten = ext4_ext_is_unwritten(path[path->p_depth].p_ext);
+
+	return ext4_split_extent_at(icb, handle, inode, ppath, lblk, unwritten ?
+			EXT4_EXT_MARK_UNWRIT1|EXT4_EXT_MARK_UNWRIT2 : 0,
+			EXT4_EX_NOCACHE | EXT4_GET_BLOCKS_PRE_IO |
+			(nofail ? EXT4_GET_BLOCKS_METADATA_NOFAIL:0));
 }
 
 /*
