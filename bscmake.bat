48a3076f99a9d7db0224f376f94eb3c4c05105ad
ngkaho1234 <ngkaho1234@gmail.com>
2015-05-15 22:13:30 +0800

    sync

--- bscmake.bat
+++
@@ -1,7 +0,0 @@
-
-set BROWSER_INFO_SUPPORTED=1
-
-build -ceZ
-"c:\Program Files (x86)\Microsoft Visual Studio\vc98\bin\BSCMAKE.EXE" /n /o winnet\chk\i386\Ext3Fsd.bsc @sbrfiles 
-
-@del /s *.sbr > NUL
